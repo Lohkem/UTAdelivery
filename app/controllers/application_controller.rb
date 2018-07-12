bfa2e83c8c1a1e35213719898341a092644d5b7d
Joe Cabezas Campos <joe.cabezas@gmail.com>
2018-07-11 20:23:29 -0700

    add to cart works, and updates cart count via ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  include CartProductsHelper
 
   before_action :require_login, except: [:index, :show]
   before_action :require_admin, only: [:new, :edit, :create, :update, :destroy]
---
+++ app/helpers/cart_products_helper.rb
@@ -0,0 +1,31 @@
+module CartProductsHelper
+  def add_product_to_cart(cart_params)
+    product_id = cart_params[:product_id]
+    quantity = cart_params[:quantity].to_i
+
+    unless shopping_cart.key? product_id
+      shopping_cart[product_id] = 0
+    end
+
+    shopping_cart[product_id] += quantity
+  end
+
+  def destroy_shopping_cart
+    session.delete[:shopping_cart]
+  end
+
+  def shopping_cart_count
+    count = 0
+    shopping_cart.each do |key, value|
+      count += value
+    end
+    count
+  end
+
+  def shopping_cart
+    unless session.key? :shopping_cart
+      session[:shopping_cart] = Hash.new
+    end
+    session[:shopping_cart]
+  end
+end
